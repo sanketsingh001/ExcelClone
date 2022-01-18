a62fc135142c7b5fa308881c6873e62b9ff2edc9
sanket singh <sanket.raj01@gmail.com>
2022-01-18 17:57:13 +0530

    Front end till Making cells

---
+++ docs/script.js
@@ -0,0 +1,47 @@
+let rows=100;
+let cols=26;
+let addressbar=document.querySelector(".address-bar")
+let addresscolcont=document.querySelector(".address-col-cont")
+let addressrowcont=document.querySelector(".address-row-cont")
+let cellscont=document.querySelector(".cells-cont");
+for(let i=0;i<rows;i++){
+    let addressCol=document.createElement("div");
+    addressCol.setAttribute("class","address-col");
+    addressCol.innerText=i+1;
+    addresscolcont.appendChild(addressCol);
+
+}
+
+
+for(let i=0;i<cols;i++){
+    let addressRow=document.createElement("div");
+    addressRow.setAttribute("class","address-row");
+    addressRow.innerText=String.fromCharCode(65+i);
+    addressrowcont.appendChild(addressRow);
+}
+
+for(let i=0;i<rows;i++ ){
+    let rowCont=document.createElement("div");
+    rowCont.setAttribute("class","rowCont");
+    for(let j=0;j<cols;j++){
+        let cell=document.createElement("div");
+        cell.setAttribute("class","cell");
+        cell.setAttribute("contenteditable","true");
+        rowCont.appendChild(cell);
+        addListenerForAddressBarDisplay(cell,i,j);
+
+
+    }
+    cellscont.append(rowCont);
+
+}
+
+function addListenerForAddressBarDisplay(cell,i,j){
+    cell.addEventListener("click",(e)=>{
+        let rowID= i+1;
+        let colID= String.fromCharCode(65+j);
+        
+addressbar.value=`${colID }${rowID}`;
+    })
+
+}
